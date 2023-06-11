0988ff2f4881c92a2dc37ebcd011d52d17ef64c5
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-06-12 00:41:55 +0200

    Scanner05.java

--- src/main/java/introduction/day03scanner/Scanner05.java
+++ src/main/java/introduction/day03scanner/Scanner05.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Scanner05 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //Kullanicidan bir dikdortgenin iki kenar uzunlugunu aliniz
-        //i) Alanini hesaplayiniz
-        //ii)Cevresini hesaplayiniz
+      
 
         System.out.print("Lütfen dikdorgenin kisa kenarini giriniz: ");
         int shortSide = input.nextInt();
@@ -17,9 +15,7 @@ public class Scanner05 {
         int cevre = 2*shortSide+2*longSide;
         System.out.println("Verilerini girmis oldugunuz dikdortgenin alani: "+alan+"\nCevresi: "+cevre);
 
-        //input.nextInt();  Eger bu sekilde yazarsan kullanicidan veri alir ama bir yere paketlemedigin icin bir ise yaramaz
-        //Bu yuzden yukaridaki gibi yap
-        //KISAYOL!!! alan.soutv yazarsan direkt System.out.println("alan = " + alan); bunu yazdirir
+        
 
 
 
