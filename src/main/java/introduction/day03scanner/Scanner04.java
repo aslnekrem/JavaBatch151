30acabad89fb30b26b9e4a4328d5944559a5687b
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-06-12 00:41:42 +0200

    Scanner04.java

--- src/main/java/introduction/day03scanner/Scanner04.java
+++ src/main/java/introduction/day03scanner/Scanner04.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Scanner04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //Kullanicidan iki sayi alip 4 islem yapan ve islemlerin sonuclarini ekrana yazdiran kodu yaziniz
+        
         System.out.println("Lütfen iki sayi giriniz: ");
         double num1 = input.nextDouble();
         double num2 = input.nextDouble();
