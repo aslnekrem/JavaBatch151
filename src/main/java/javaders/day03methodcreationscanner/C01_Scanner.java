c881091f8a02391211e53deb4fd63b18639aa8f4
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-06-13 01:19:50 +0200

    C01_Scanner.java

--- src/main/java/javaders/day03methodcreationscanner/C01_Scanner.java
+++ src/main/java/javaders/day03methodcreationscanner/C01_Scanner.java
@@ -6,35 +6,24 @@ import java.util.Scanner;
 public class C01_Scanner {
     public static void main(String[] args) {
 
-        /*
-
-        Kullanicidan data almak icin Scanner Classi kullanilir.Ve bir input/ scan/ scanner Objesi olusturulur
-        .Bu objeyi new objeyi new key worduyle olustururuz
-
-     */
-        // 1 )Adim Object Olusturma
+       
         Scanner input = new Scanner(System.in);//Tolgahan
 
-        //2 ) Kullaniciya ne yapacagini soyluyoruz
         System.out.println("Lutfen maasinizi giriniz");
 
-        // 3 ) Adim kullanicidan alinan datalar yapisina ve
-        // buyuklugune gore bir Variable'a yerlestirilir
+        
         long maas = input.nextLong();
-        // 4) Kullanicini verdigi bilgileri teyit edebilmesi vs. icin yazdiriyoruz
 
         System.out.println("maas = " + maas);
 
 
-        // Kullaniciya cinsiyetini, adini, memleketini , yasini,boyunu,
-        // ulkesini sevip sevmedigini soran bir program yaziniz
+       
 
         System.out.println("Lutfen cinsiyetinizi Kadin ise K Erkek ise E olarak giriniz");
         char cinsiyet = input.next().charAt(0);//erkek
 
 
         System.out.println("Lutfen adinizi giriniz");
-        // String isim = input.next();
         String name = input.next();//Tolgahan Metehan
 
         System.out.println("Lutfen memleketinizi giriniz");
