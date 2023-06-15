1d8e4bc0b4979d512d3b0772920dd9d371465e24
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-06-15 19:37:32 +0200

    C03_TypeCasting.java

--- src/main/java/javaders/day05typecastingasciivaluestringmanupulations/C03_TypeCasting.java
+++ src/main/java/javaders/day05typecastingasciivaluestringmanupulations/C03_TypeCasting.java
@@ -3,19 +3,7 @@ package javaders.day05typecastingasciivaluestringmanupulations;
 public class C03_TypeCasting {
     public static void main(String[] args) {
 
-        /*
-
-        Numeric data type'larinin birbirine donusturulmesine Type Casting denir.
-        Numeric data Type'lari ==>  byte<short<int<long<float<double
-
-        Kucuk Data Type'larinin buyuk data type'larina donusturulmesine Auto Widening (Otomatik Genisletme) == Kucuk
-        Kutunun Buyuk Kutu icerisine yerlesttirilmesi
-
-
-        Buyuk Kutu nun Kucuk Kutu'ya donusturulmesinde Java sorumlulugu kabul etmez. Data kaybi olabilecegi icin
-        kodu yazanlara birakir bu sorumlulugu. Buna Explicit Narrowing (Aciktan Daraltma) denir.
-
-         */
+       
 
         byte age = 13;
         int ageInt = age;  //Auto Widening. Bu kadar basit.
