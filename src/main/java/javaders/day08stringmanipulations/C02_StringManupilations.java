970760ff2c40d15ea4e909bee17f7c5c42d0e297
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-06-17 18:14:25 +0200

    C02_StringManupilations.java

--- src/main/java/javaders/day08stringmanipulations/C02_StringManupilations.java
+++ src/main/java/javaders/day08stringmanipulations/C02_StringManupilations.java
@@ -2,7 +2,7 @@ package javaders.day08stringmanipulations;
 
 public class C02_StringManupilations {
     public static void main(String[] args) {
-        //****************      replaceFrist()      //////////
+       
 
         
 
