b45a8e3c71fa7a8184d5c9047b306999d11a8fc8
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-07-06 20:45:13 +0200

    C04_Dikdortgen.java

--- src/main/java/practices/C04_Dikdortgen.java
+++ src/main/java/practices/C04_Dikdortgen.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class C04_Dikdortgen {
     public static void main(String[] args) {
-        // Kullanicidan dikdortgenin a ve b kenarlarinin uzunluklarini ayri ayri sisteme girmesini isteyiniz.
         // ve Dikdorgenin cevresini hesaplayarak ekrana yazdiriniz
 
         Scanner scan = new Scanner(System.in);
