416ed54703278634c33bd9e02708bc59b854d593
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-07-08 20:49:19 +0200

    Arrays04.java

--- src/main/java/javaders/day16arraysforeachloop/Arrays04.java
+++ src/main/java/javaders/day16arraysforeachloop/Arrays04.java
@@ -6,7 +6,6 @@ public class Arrays04 {
 
     public static void main(String[] args) {
 
-        //Size verilen bir cumlede kac kelime oldugunu gosteren kodu yaziniz.
         String s = "Java is easy. Learn Java earn money.";
         String words[] = s.split(" ");
         System.out.println(Arrays.toString(words));// [Java, is, easy., Learn, Java, earn, money.]
