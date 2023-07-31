77cfd777621dc075df66b45e881d995c5b3919f8
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-07-31 19:39:37 +0200

    Scanner02.java

--- src/main/java/introduction/day03scanner/Scanner02.java
+++ src/main/java/introduction/day03scanner/Scanner02.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Scanner02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
 
         System.out.println("Ilk isminizi giriniz: ");
         String firstname = input.next();
