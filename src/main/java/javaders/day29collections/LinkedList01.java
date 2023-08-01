e1c49513871c11674f7cc2b623554f1f966a6f43
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-08-01 19:58:43 +0200

    LinkedList01.java

--- src/main/java/javaders/day29collections/LinkedList01.java
+++ src/main/java/javaders/day29collections/LinkedList01.java
@@ -6,6 +6,7 @@ public class LinkedList01 {
     
     public static void main(String[] args) {
         
+        
     /*
         ArrayList'ler eleman silme ve ekleme'de tekrar index'leme yapmak zorunda olduklarindan, eleman ekleme ve silmede
             basarisizdirlar
