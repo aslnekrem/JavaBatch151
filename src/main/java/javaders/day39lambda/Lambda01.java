a775f50e84a2bf17692a368eebc7067fd7039de2
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-08-09 10:13:03 +0200

    Lambda01.java

--- src/main/java/javaders/day39lambda/Lambda01.java
+++ src/main/java/javaders/day39lambda/Lambda01.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Lambda01 {
     public static void main(String[] args) {
-        
+    
 
         List<String> myList = new ArrayList<>();
         myList.add("Tom");
