9a1665cf686d1e754a2b21914ff97fde21f49fec
aslnekrem <130939322+aslnekrem@users.noreply.github.com>
2023-08-13 15:15:06 +0200

    Lambda01.java

--- src/main/java/javaders/day44lambda/Lambda01.java
+++ src/main/java/javaders/day44lambda/Lambda01.java
@@ -13,6 +13,7 @@ public class Lambda01 {
 
     
     public static void main(String[] args) throws IOException {
+
         
         //Example 1: Verilen text file icindeki text'i console'a yazdiran code'u yaziniz.
         Files.lines(Paths.get("src/main/java/javaders/day44lambda/myTextFile.txt")).forEach(System.out::println);
